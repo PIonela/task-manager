fb67e8b37f6e1e5031fdf05852a3865991c8548f
Ionela <pascaruionela21@gmail.com>
2024-07-11 21:39:05 +0300

    create context & task filter wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+// import { createContext } from "react";
 import ContainerBox from "./components/ContainerBox/ContainerBox";
+import { TaskProvider } from "./context/TaskContext";
+// export const TaskContext = createContext();
 
 function App() {
   return (
     <div className="App">
-      <ContainerBox />
+      {/* <TaskContext.Provider value={[1, 2, 3]}> */}
+      <TaskProvider>
+        <ContainerBox />
+      </TaskProvider>
+      {/* </TaskContext.Provider> */}
     </div>
   );
 }
--- src/components/ContainerBox/ContainerBox.jsx
+++ src/components/ContainerBox/ContainerBox.jsx
@@ -5,39 +5,11 @@ import "./ContainerBox.css";
 import NoTasks from "../NoTasks/NoTasks";
 import Modal from "../Modal/Modal";
 import ControlPanel from "../ControlPanel/ControlPanel";
+import { useTaskContext } from "../../context/TaskContext";
 
 const ContainerBox = () => {
-  const data = [
-    {
-      taskName: "task1",
-      taskDate: "12-12-2024",
-      taskDescription: "desc1",
-      taskStatus: "Todo",
-    },
-
-    {
-      taskName: "task2",
-      taskDate: "12-12-2024",
-      taskDescription: "desc2",
-      taskStatus: "Pending",
-    },
-
-    {
-      taskName: "task3",
-      taskDate: "12-12-2024",
-      taskDescription: "desc3",
-      taskStatus: "In Progress",
-    },
-
-    {
-      taskName: "task4",
-      taskDate: "12-12-2024",
-      taskDescription: "desc4",
-      taskStatus: "Completed",
-    },
-  ];
-  const [taskList, setTaskList] = useState(data);
   const [isMOpen, setIsMOpen] = useState(false);
+  const { taskList, setTaskList } = useTaskContext();
 
   const openModal = () => setIsMOpen(true);
   const closeModal = () => setIsMOpen(false);
@@ -59,6 +31,7 @@ const ContainerBox = () => {
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(taskList));
   }, [taskList]);
+
   return (
     <div className="container-box">
       <ControlPanel onClickFunction={openModal} />
